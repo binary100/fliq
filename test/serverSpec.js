33fbae32150c30417f097335ccd4aba384064f65
Rob Cornell <rob.cornell@gmail.com>
2017-07-08 19:09:51 -0400

    feat($test): Create first test and scaffolding

--- test/serverSpec.js
+++ test/serverSpec.js
@@ -1,3 +1,62 @@
 const request = require('supertest');
 const asset = require('chai').assert;
 const expect = require('chai').expect;
+
+const serverUrl = `http://localhost:3000`;
+
+describe('FlickPick tests', function() {
+
+  describe('Server stability', function() {
+    it('should respond to requests', function(done) {
+      request(serverUrl)
+        .get('')
+        .expect(200)
+        .end(function(err, res) {
+          if (err) throw err;
+          done();
+        });
+    });
+  });
+});
+
+
+
+// // Methods for stubbing HTTP requests and responses
+// module.exports = {
+
+//   response: function() {
+//     this._ended = false;
+//     this._responseCode = null;
+//     this._headers = null;
+//     this._data = null;
+
+//     this.writeHead = function(responseCode, headers) {
+//       this._responseCode = responseCode;
+//       this._headers = headers;
+//     }.bind(this);
+
+//     this.end = function(data) {
+//       this._ended = true;
+//       this._data = data;
+//     }.bind(this);
+//   },
+
+//   request: function(url, method, postdata) {
+//     this.url = url;
+//     this.method = method;
+//     this._postData = postdata;
+//     this.setEncoding = function() { /* noop */ };
+
+//     this.addListener = this.on = function(type, callback) {
+//       if (type === 'data') {
+//         callback(JSON.stringify(this._postData));
+//       }
+
+//       if (type === 'end') {
+//         callback();
+//       }
+
+//     }.bind(this);
+//   }
+
+// };
