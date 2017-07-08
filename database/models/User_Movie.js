4ee1c61e461385f804b452c72583ad1b36ee9d0b
Hack Reactor Student <student@hackreactor.com>
2017-07-08 16:24:49 -0400

    Join tables pre-test commit

--- database/models/User_Movie.js
+++ database/models/User_Movie.js
@@ -9,4 +9,13 @@ const User_Movie = db.define('User_Movie', {});
 User.belongsToMany(Movie, {through: User_Movie, as: 'user_id', foreignKey: 'id'});
 Movie.belongsToMany(User, {through: User_Movie, as: 'movie_id', foreignKey: 'id'});
 
+// User_Movie.sync().then((err) => {
+//   if (err) {
+//     console.error('Error creating User_Tag table', err);
+//   } else {
+//     console.log('User_Tag table created successfully');
+//   }
+// });
+
+
 module.exports = User_Movie;
