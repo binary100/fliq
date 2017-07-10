0687b94dde57263ea3d7666ace6eee62a3c5b90d
Rob Cornell <rob.cornell@gmail.com>
2017-07-10 19:24:10 -0400

    style($style): 16by9 the trailer and add top margin to line up

--- public/src/components/trailer.jsx
+++ public/src/components/trailer.jsx
@@ -3,12 +3,18 @@ import React from 'react';
 const youtubeUrl = 'https://www.youtube.com/embed/';
 
 const Trailer = ({ trailer }) => (
-  <div>
-    <iframe
-      className="embed-responsive-item"
-      src={trailer ? youtubeUrl + trailer.id.videoId : ''}
-      allowFullScreen
-    />
+  <div className="container">
+    <div className="row">
+      <div className="col-sm-6">
+        <div className="embed-responsive embed-responsive-16by9 trailer">
+          <iframe
+            className="embed-responsive-item"
+            src={trailer ? youtubeUrl + trailer.id.videoId : ''}
+            allowFullScreen
+          />
+        </div>
+      </div>
+    </div>
   </div>
 );
 
