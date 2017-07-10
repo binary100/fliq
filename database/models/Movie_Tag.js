e4a22f73f9ec8f76e239824eb6f12e1a91dbf192
Rob Cornell <rob.cornell@gmail.com>
2017-07-10 15:03:34 -0400

    refactor($db): Comment out syncs per Tae

--- database/models/Movie_Tag.js
+++ database/models/Movie_Tag.js
@@ -9,12 +9,12 @@ const Movie_Tag = db.define('Movie_Tag', {});
 Movie.belongsToMany(Tag, {through: Movie_Tag, as: 'movie_id', foreignKey: 'id'});
 Tag.belongsToMany(Movie, {through: Movie_Tag, as: 'tagId', foreignKey: 'id'});
 
-Movie_Tag.sync().then((err) => {
-  if (err) {
-    console.error('Error creating Movie_Tag table', err);
-  } else {
-    console.log('Movie_Tag table created successfully');
-  }
-});
+// Movie_Tag.sync().then((err) => {
+//   if (err) {
+//     console.error('Error creating Movie_Tag table', err);
+//   } else {
+//     console.log('Movie_Tag table created successfully');
+//   }
+// });
 
 module.exports = Movie_Tag;
--- database/models/User_Movie.js
+++ database/models/User_Movie.js
@@ -14,13 +14,13 @@ const User_Movie = db.define('User_Movie', {
 });
 
 
-User_Movie.sync().then((err) => {
-  if (err) {
-    console.error('Error creating User_Tag table', err);
-  } else {
-    console.log('User_Tag table created successfully');
-  }
-});
+// User_Movie.sync().then((err) => {
+//   if (err) {
+//     console.error('Error creating User_Tag table', err);
+//   } else {
+//     console.log('User_Tag table created successfully');
+//   }
+// });
 
 User.belongsToMany(Movie, {through: User_Movie, as: 'userId', foreignKey: 'id'});
 Movie.belongsToMany(User, {through: User_Movie, as: 'movieId', foreignKey: 'id'});
