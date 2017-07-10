54e5e945df26a37c0156bdf4d577c5cf8d1b8f08
David Earle <davidearle@Davids-MacBook-Pro.local>
2017-07-10 11:21:20 -0400

    docs(none): Changes to personal docs

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
-const express = require('express');
-
 require('dotenv').config();
+const express = require('express');
 const db = require('./database/dbsetup.js');
 
 const bodyParser = require('body-parser');
