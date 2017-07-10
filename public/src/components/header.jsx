f03d5e04eee1400a47c33da21408bfd01fc8fbba
Rob Cornell <rob.cornell@gmail.com>
2017-07-09 23:37:27 -0400

    refactor($auth): Style adjustments to user photo

--- public/src/components/header.jsx
+++ public/src/components/header.jsx
@@ -2,15 +2,18 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Header = props => (
-  <div className="header">
+  <div className="navbar-inner header">
     <Link className="link header-link home" to="/" >Home</Link>
     <Link className="link header-link results" to="/results">Results</Link>
     <span className="user-info">
-    <span className="user-photo">
-      <img src={props.user ? props.user.picture : ''} />
-    </span>
+      <span>
+        <img
+          className="img-circle user-photo"
+          src="https://lh4.googleusercontent.com/-QpbHKV1gzhM/AAAAAAAAAAI/AAAAAAAAIn4/ow5QMLq7VFI/photo.jpg?sz=50"
+        />
+      </span>
     </span>
   </div>
 );
-
+//<img src={props.user ? props.user.picture : ''} />
 export default Header;
