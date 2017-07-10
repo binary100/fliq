823bfc311a65aab3c9d05f3595a0257d119cc1e5
Rob Cornell <rob.cornell@gmail.com>
2017-07-10 13:06:04 -0400

    feat($style): Add bootstrap glyphs

--- public/src/components/header.jsx
+++ public/src/components/header.jsx
@@ -3,21 +3,40 @@ import { Link } from 'react-router-dom';
 
 const Header = props => (
   <div className="navbar-inner header">
-    <Link className="link header-link home" to="/" >Home</Link>
-    <Link className="link header-link results" to="/results">Results</Link>
-    <Link className="link header-link results" to="/" onClick={props.handleLogout} >
-      Logout
-    </Link>
+    <span className="glyphicon glyphicon-menu-hamburger header-icon" />
+    <span className="btn-group" role="group" >
+      <Link className="link header-link home" to="/" >
+        <button type="button" className="btn btn-default">Home</button>
+      </Link>
+      <Link className="link header-link results" to="/results">
+        <button type="button" className="btn btn-default">Results</button>
+      </Link>
+      <Link className="link header-link results" to="/" onClick={props.handleLogout} >
+        Logout
+      </Link>
+    </span>
+
     <span className="user-info">
-      <span>
-        <img
-          className="img-circle user-photo"
-          src={props.user ? props.user.picture : ''}
-          alt={props.user ? props.user.name : ''}
-        />
+      <img
+        className="img-circle user-photo"
+        src={props.user ? props.user.picture : ''}
+        alt={props.user ? props.user.name : '' }
+      />
+      <span className="pull-right">
+        {props.user ? props.user.name : ''}
       </span>
     </span>
   </div>
 );
 
 export default Header;
+
+// <img
+//   className="img-circle user-photo"
+//   src={props.user ? props.user.picture : ''}
+//  alt={props.user ? props.user.name : '' }
+///>
+
+//<button type="button" className="btn btn-default btn-lg">
+  // <span className="glyphicon glyphicon-star" aria-hidden="true"></span> Star
+// </button>
