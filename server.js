d8fe441f3936b096ac3536f1c6b67d0efd1b46d1
Rob Cornell <rob.cornell@gmail.com>
2017-07-10 17:32:22 -0400

    fix(Fix User reference in server.js):

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 require('dotenv').config();
-const db = require('./database/dbsetup.js');
+const User = require('./database/dbsetup.js').users;
 
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
