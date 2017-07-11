9f46ebcbdc4090cca1f72e21fb2c729b036992be
Rob Cornell <rob.cornell@gmail.com>
2017-07-11 11:31:16 -0400

    refactor($header): Un-hardcode user phto/nam

--- public/src/components/header.jsx
+++ public/src/components/header.jsx
@@ -4,7 +4,6 @@ import { Link } from 'react-router-dom';
 const Header = props => (
   <div className="navbar-inner header">
     <span className="glyphicon glyphicon-menu-hamburger header-icon" />
-
     <span className="btn-group" role="group" >
       <Link className="link header-link home" to="/" >
         <button type="button" className="btn btn-default">Home</button>
@@ -13,14 +12,14 @@ const Header = props => (
         <button type="button" className="btn btn-default">Results</button>
       </Link>
     </span>
-
     <span className="user-info">
       <img
         className="img-circle user-photo pull-right"
-        src="https://lh4.googleusercontent.com/-QpbHKV1gzhM/AAAAAAAAAAI/AAAAAAAAIn4/ow5QMLq7VFI/photo.jpg?sz=50"
+        src={props.user ? props.user.picture : ''}
+       alt={props.user ? props.user.name : '' }
       />
       <span className="pull-right">
-        {props.user ? props.user.name : 'No Name'}
+        {props.user ? props.user.name : ''}
       </span>
     </span>
   </div>
@@ -28,11 +27,21 @@ const Header = props => (
 
 export default Header;
 
-// <img
-//   className="img-circle user-photo"
-//   src={props.user ? props.user.picture : ''}
-//  alt={props.user ? props.user.name : '' }
-///>
+
+/*
+<img
+  className="img-circle user-photo pull-right"
+  src="https://lh4.googleusercontent.com/-QpbHKV1gzhM/AAAAAAAAAAI/AAAAAAAAIn4/ow5QMLq7VFI/photo.jpg?sz=50"
+/>
+
+<img
+  className="img-circle user-photo"
+  src={props.user ? props.user.picture : ''}
+ alt={props.user ? props.user.name : '' }
+/>
+
+*/
+
 
 //<button type="button" className="btn btn-default btn-lg">
   // <span className="glyphicon glyphicon-star" aria-hidden="true"></span> Star
--- public/src/components/smallMovieTile.jsx
+++ public/src/components/smallMovieTile.jsx
@@ -5,9 +5,17 @@ const ResultsSmallTile = ({ movie, handleTileClick }) => (
     className="col-sm-2 small-movie-tile"
     onClick={(e, evt) => handleTileClick(e, evt, movie)}
   >
+
     <p>{movie.title} ({movie.year})</p>
     <img className="poster-small" src={movie.poster} alt="Poster" />
+
   </div>
 );
 
 export default ResultsSmallTile;
+
+// <span className="col-sm-6">
+    //   <p>{movie.director}</p>
+    //   <p>{movie.writer}</p>
+    //   <p>{movie.rated}</p>
+    // </span>
