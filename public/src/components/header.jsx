6edec5f248975e9d26a6324e1096bf2d695b699d
Rob Cornell <rob.cornell@gmail.com>
2017-07-11 13:59:29 -0400

    style($style): Begin header refactor to use bootstrap

--- public/src/components/header.jsx
+++ public/src/components/header.jsx
@@ -2,7 +2,52 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Header = props => (
-  <div className="navbar-inner header">
+
+  <nav className="navbar navbar-default header">
+    <div className="container-fluid">
+      <div className="navbar-header">
+        <ul className="nav navbar-nav">
+        </ul>
+        <span className="glyphicon glyphicon-menu-hamburger header-icon" />
+        <span className="btn-group" role="group" >
+          <Link className="link header-link home" to="/" >
+            <button type="button" className="btn btn-default">Home</button>
+          </Link>
+          <Link className="link header-link results" to="/results">
+            <button type="button" className="btn btn-default">Results</button>
+          </Link>
+        </span>
+        <span className="user-info">
+          <img
+            className="img-circle user-photo pull-right"
+            src={props.user ? props.user.picture : ''}
+           alt={props.user ? props.user.name : '' }
+          />
+          <span className="pull-right">
+            {props.user ? props.user.name : ''}
+          </span>
+        </span>
+      </div>
+    </div>
+  </nav> 
+);
+
+export default Header;
+
+
+/*
+<img
+  className="img-circle user-photo pull-right"
+  src="https://lh4.googleusercontent.com/-QpbHKV1gzhM/AAAAAAAAAAI/AAAAAAAAIn4/ow5QMLq7VFI/photo.jpg?sz=50"
+/>
+
+<img
+  className="img-circle user-photo"
+  src={props.user ? props.user.picture : ''}
+ alt={props.user ? props.user.name : '' }
+/>
+
+<div className="navbar-inner header">
     <span className="glyphicon glyphicon-menu-hamburger header-icon" />
     <span className="btn-group" role="group" >
       <Link className="link header-link home" to="/" >
@@ -23,22 +68,6 @@ const Header = props => (
       </span>
     </span>
   </div>
-);
-
-export default Header;
-
-
-/*
-<img
-  className="img-circle user-photo pull-right"
-  src="https://lh4.googleusercontent.com/-QpbHKV1gzhM/AAAAAAAAAAI/AAAAAAAAIn4/ow5QMLq7VFI/photo.jpg?sz=50"
-/>
-
-<img
-  className="img-circle user-photo"
-  src={props.user ? props.user.picture : ''}
- alt={props.user ? props.user.name : '' }
-/>
 
 */
 
