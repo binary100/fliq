7b69dfaa73e68328128b57e089522271d49bdf05
Rob Cornell <rob.cornell@gmail.com>
2017-07-10 20:14:48 -0400

    feat($results): Change selected movie details and trailer on tile click

--- public/src/components/smallMovieTile.jsx
+++ public/src/components/smallMovieTile.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 
-const ResultsSmallTile = ({ movie }) => (
-  <div className="col-sm-2 small-movie-tile">
+const ResultsSmallTile = ({ movie, handleTileClick }) => (
+  <div
+    className="col-sm-2 small-movie-tile"
+    onClick={(e, evt) => handleTileClick(e, evt, movie)}
+  >
     <p>{movie.title}</p>
-    <img className="poster" src={movie.poster} />
+    <img className="poster" src={movie.poster} alt="Poster" />
   </div>
 );
 
