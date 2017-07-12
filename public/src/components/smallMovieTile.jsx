96b8411eff86dbd9e03f9b4be4f258930f134b63
Rob Cornell <rob.cornell@gmail.com>
2017-07-12 18:09:30 -0400

    style($style): Create placeholder style for search like/seen buttons

--- public/src/components/smallMovieTile.jsx
+++ public/src/components/smallMovieTile.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
 
 const SmallMovieTile = ({ movie, selectSmallTile }) => (
   <div
-    className="col-sm-2 small-movie-tile"
+    className="col-sm-3 small-movie-tile"
     onClick={(e, evt) => selectSmallTile(e, evt, movie)}
   >
+
     <p>{movie.title} ({movie.year})</p>
-    <img className="poster-small" src={movie.poster} alt="Poster" />
+    <div>
+      <img className="poster-small col-sm-6" src={movie.poster} alt="Poster" />
+      <span className="col-sm-6">
+      <div className="row">
+        <Button className="col-sm-10 small-tile-button">Like It</Button>
+      </div>
+      <div className="row">
+        <Button className="col-sm-10 small-tile-button">Seen It</Button>
+      </div>
+      </span>
+    </div>
   </div>
 );
 
