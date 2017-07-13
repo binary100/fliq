bb7ea1170909deb89960c96483acfd34f2c404f7
Rob Cornell <rob.cornell@gmail.com>
2017-07-13 15:49:56 -0400

    refactor($buttons): Refactor like/saw buttons into one dumb component

---
+++ public/src/components/loadingButton.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Button } from 'react-bootstrap';
+
+const LoadingButton = ({ handleClick, buttonText}) => {
+  return (
+    <Button
+      className="col-sm-10 small-tile-button"
+      onClick={handleClick}
+    >
+      { buttonText }
+    </Button>
+  );
+};
+
+export default LoadingButton;
