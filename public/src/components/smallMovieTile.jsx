a0cf7b9727505ba0374796edb0e41c24aad33faf
Rob Cornell <rob.cornell@gmail.com>
2017-07-13 15:49:56 -0400

    refactor($buttons): Refactor like/saw buttons into one dumb component

--- public/src/components/smallMovieTile.jsx
+++ public/src/components/smallMovieTile.jsx
@@ -1,35 +1,50 @@
 import React from 'react';
-import LikeButton from './likeButton.jsx';
-import SeenButton from './seenButton.jsx';
+import LoadingButton from './loadingButton.jsx';
 import axios from 'axios';
 
 class SmallMovieTile extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      likeButtonText: 'Like!',
-      sawButtonText: 'Saw it!'
+      likeButtonText: 'I like it!',
+      sawButtonText: 'I saw it!',
+      canClickLike: true,
+      canClickSaw: true
     };
     this.likeMovie = this.likeMovie.bind(this);
     this.sawMovie = this.sawMovie.bind(this);
   }
 
   likeMovie(movie) {
-    this.setState({ likeButtonText: 'Processing...' });
+    if (!this.state.canClickLike) return;
+    this.setState({
+      likeButtonText: 'Processing...',
+      canClickLike: false
+    });
     axios.post('/api/movie/like', {
       movie
     })
       .then(() => this.setState({ likeButtonText: 'Saved!' }))
-      .catch(err => console.error('Error marking as seen: ', err));
+      .catch((err) => {
+        console.error('Error marking as liked: ', err);
+        this.setState({ likeButtonText: 'Error :(' });
+      });
   }
 
   sawMovie(movie) {
-    this.setState({ sawButtonText: 'Processing...' });
+    if (!this.state.canClickSaw) return;
+    this.setState({
+      sawButtonText: 'Processing...',
+      canClickSaw: false
+    });
     axios.post('/api/movie/saw', {
       movie
     })
       .then(() => this.setState({ sawButtonText: 'Saved!' }))
-      .catch(err => console.error('Error marking as seen: ', err));
+      .catch((err) => {
+        console.error('Error marking as seen: ', err);
+        this.setState({ sawButtonText: 'Error :(' });
+      });
   }
 
   render() {
@@ -50,15 +65,15 @@ class SmallMovieTile extends React.Component {
           />
           <span className="col-sm-6">
             <div className="row">
-              <LikeButton
+              <LoadingButton
                 buttonText={this.state.likeButtonText}
-                likeMovie={this.likeMovie}
+                handleClick={this.likeMovie}
               />
             </div>
             <div className="row">
-              <SeenButton
+              <LoadingButton
                 buttonText={this.state.sawButtonText}
-                sawMovie={this.sawMovie}
+                handleClick={this.sawMovie}
               />
             </div>
           </span>
