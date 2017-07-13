a9cc2b115f70998134b976b3404e71d7a3e75f21
Rob Cornell <rob.cornell@gmail.com>
2017-07-13 15:38:22 -0400

    refactor(Convert smallMovieTile into stateful component):

--- public/src/components/smallMovieTile.jsx
+++ public/src/components/smallMovieTile.jsx
@@ -1,28 +1,72 @@
 import React from 'react';
 import LikeButton from './likeButton.jsx';
 import SeenButton from './seenButton.jsx';
-import { Button } from 'react-bootstrap';
+import axios from 'axios';
 
+class SmallMovieTile extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      likeButtonText: 'Like!',
+      sawButtonText: 'Saw it!'
+    };
+    this.likeMovie = this.likeMovie.bind(this);
+    this.sawMovie = this.sawMovie.bind(this);
+  }
 
-const SmallMovieTile = ({ movie, selectSmallTile }) => (
-  <div
-    className="col-sm-3 small-movie-tile"
-    onClick={(e, evt) => selectSmallTile(e, evt, movie)}
-  >
+  likeMovie(movie) {
+    this.setState({ likeButtonText: 'Processing...' });
+    axios.post('/api/movie/like', {
+      movie
+    })
+      .then(() => this.setState({ likeButtonText: 'Saved!' }))
+      .catch(err => console.error('Error marking as seen: ', err));
+  }
 
-    <p>{movie.title} ({movie.year})</p>
-    <div>
-      <img className="poster-small col-sm-6" src={movie.poster} alt="Poster" />
-      <span className="col-sm-6">
-      <div className="row">
-        <LikeButton />
+  sawMovie(movie) {
+    this.setState({ sawButtonText: 'Processing...' });
+    axios.post('/api/movie/saw', {
+      movie
+    })
+      .then(() => this.setState({ sawButtonText: 'Saved!' }))
+      .catch(err => console.error('Error marking as seen: ', err));
+  }
+
+  render() {
+    return (
+      <div
+        className="col-sm-3 small-movie-tile"
+        onClick={(e, evt) => this.props.selectSmallTile(e, evt, this.props.movie)}
+      >
+        <p
+          className="small-movie-tile-title">
+            {this.props.movie.title} ({this.props.movie.year})
+        </p>
+        <div>
+          <img
+            className="poster-small col-sm-6"
+            src={this.props.movie.poster}
+            alt="Poster"
+          />
+          <span className="col-sm-6">
+            <div className="row">
+              <LikeButton
+                buttonText={this.state.likeButtonText}
+                likeMovie={this.likeMovie}
+              />
+            </div>
+            <div className="row">
+              <SeenButton
+                buttonText={this.state.sawButtonText}
+                sawMovie={this.sawMovie}
+              />
+            </div>
+          </span>
+        </div>
       </div>
-      <div className="row">
-        <SeenButton />
-      </div>
-      </span>
-    </div>
-  </div>
-);
+    );
+  }
+
+}
 
 export default SmallMovieTile;
