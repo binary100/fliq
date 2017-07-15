af71510ff132f11ff5e83671c7ca6ca838c32a5f
Rob Cornell <rob.cornell@gmail.com>
2017-07-15 11:55:44 -0400

    feat($movienight): Add basic components to movie night

--- public/src/containers/movieNight.jsx
+++ public/src/containers/movieNight.jsx
@@ -1,17 +1,50 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
+
+const subHeader = `FLIQ's recommendation engine can aggregate several people's preferences to suggest 
+  movies that the group may enjoy. Enter other users' email addresses below and search for movies to watch.`;
 
 class MovieNight extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      confirmText: '',
+      inputText: '',
+      emails: ['rob.cornell@gmail.com', 'jac@gmail.com', 'doctor@gmail.com']
+    };
   }
 
   render() {
     return (
       <div className="container fadeIn">
-        Hi
+
+        <div className="row">
+          <div className="col-sm-12">
+            <h2>Movie Night</h2>
+            <h4>{subHeader}</h4>
+          </div>
+        </div>
+
+        <div className="row email-inputs">
+          <div className="col-sm-3 email-input-box">
+            input goes here
+          </div>
+          <Button className="col-sm-1 email-input-button">
+            button
+          </Button>
+        </div>
+
+        <div className="row">
+          <div className="col-sm-6 email-box">
+            <ul>
+              {this.state.emails.map(email => <li>{email}</li>)}
+            </ul>
+          </div>
+        </div>
+
       </div>
-    )
+    );
   }
 }
 
-export default MovieNight
+export default MovieNight;
