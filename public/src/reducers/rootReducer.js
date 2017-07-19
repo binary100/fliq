c622894a33bab02779a859d96925045c0143107b
Andrew Shih <acs@Andrews-MacBook-Pro.local>
2017-07-19 14:27:48 -0400

    chore($merge): Resolve merge conflict

--- public/src/reducers/rootReducer.js
+++ public/src/reducers/rootReducer.js
@@ -1,18 +1,10 @@
 import { combineReducers } from 'redux';
 import auth from './loginReducer.js';
-<<<<<<< HEAD
-import toggleUserSettings from './settingsReducer.js';
-
-const reducer = combineReducers({
-  auth,
-  toggleUserSettings
-=======
 import userSettingsReducer from './settingsReducer.js';
 
 const reducer = combineReducers({
   auth,
   userSettingsReducer
->>>>>>> userDashboardBackEnd
 });
 
 export default reducer;
