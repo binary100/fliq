52742f3d09ec443f4f33ecbfce000339a39b3e79
Rob Cornell <rob.cornell@gmail.com>
2017-07-20 11:27:28 -0400

    style(Add hover color to lightning tiles):

--- public/src/components/largeMovieTile.jsx
+++ public/src/components/largeMovieTile.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import MovieDetails from './movieDetails.jsx';
 
-const LargeMovieTile = ({ movie, handleLightningTileClick }) => (
-  <span className="col-sm-6 large-movie-tile">
+const baseClass = `col-sm-6 large-movie-tile`;
+
+const LargeMovieTile = ({ movie, handleLightningTileClick, isLightning }) => (
+  <span className={baseClass + (isLightning ? ' lightning-tile' : '')}>
     <span onClick={(e, evt) => handleLightningTileClick(e, evt, movie)}>
       <MovieDetails movie={movie} />
     </span>
