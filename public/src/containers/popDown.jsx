77ceed843419c5449ef11b79c7b743f52856f88d
Rob Cornell <rob.cornell@gmail.com>
2017-07-21 17:18:36 -0400

    style($popdown): Add style and state to allow for slideUp
    This cannot currently be implemented while the popDown does not know what its child is

--- public/src/containers/popDown.jsx
+++ public/src/containers/popDown.jsx
@@ -4,13 +4,25 @@ import React from 'react';
 class PopDown extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      animationClass: 'popdown-slideDown'
+    };
+  }
+
+  // This can only be used if the popDown is specific
+  // to the subcomponent, since this method can't be passed down
+  // With an unknown component
+  hidePopDown() {
+    this.setState({
+      animationClass: 'popdown-slideUp'
+    });
   }
 
   render() {
 
     return (
       <div className="container">
-        <div className="popdown popdown-slideDown">
+        <div className={`popdown ${this.state.animationClass}`}>
           {this.props.component}
         </div>
       </div>
