15a119e7a7a1ecd8fb84285aa6e55503d2d3cfc7
Rob Cornell <rob.cornell@gmail.com>
2017-07-22 16:10:04 -0400

    fix($search): Add handling for null values in search results

--- public/src/components/searchResultsTable.jsx
+++ public/src/components/searchResultsTable.jsx
@@ -3,28 +3,32 @@ import SmallMovieTile from './smallMovieTile.jsx';
 
 const SearchResultsTable = ({ movies, selectSmallTile }) => {
   let count = 0;
-  let arr = [];
-  for (var i = 0; i < movies.length; i += 2) {
+  const arr = [];
+  for (let i = 0; i < movies.length; i += 2) {
     const result = (
       <div key={count += 1}>
         <div>
           <span className="col-sm-6">
-            <SmallMovieTile
-              key={count += 1}
-              showButtons
-              selectSmallTile={selectSmallTile}
-              movie={movies[i]}
-              fromSearch
-            />
+            { movies[i] &&
+              <SmallMovieTile
+                key={count += 1}
+                showButtons
+                selectSmallTile={selectSmallTile}
+                movie={movies[i]}
+                fromSearch
+              />
+            }
           </span>
           <span className="col-sm-6">
-            <SmallMovieTile
-              key={count += 1}
-              showButtons
-              selectSmallTile={selectSmallTile}
-              movie={movies[i + 1]}
-              fromSearch
-            />
+            { movies[i + 1] &&
+              <SmallMovieTile
+                key={count += 1}
+                showButtons
+                selectSmallTile={selectSmallTile}
+                movie={movies[i + 1]}
+                fromSearch
+              />
+            }
           </span>
         </div>
       </div>
