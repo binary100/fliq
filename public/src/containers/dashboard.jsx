8f4a10eff9fd79510f61e4400620db6addf2f5f2
Andrew Shih <acs@Andrews-MacBook-Pro.local>
2017-07-24 19:55:53 -0400

    feat($dashboard): Display dropdown menu for charts; component remains unconnected

--- public/src/containers/dashboard.jsx
+++ public/src/containers/dashboard.jsx
@@ -48,6 +48,8 @@ class Dashboard extends React.Component {
     this.absoluteChartsDropDownHandler = this.absoluteChartsDropDownHandler.bind(this);
     // this.relativeChartsDropDownHandler = this.relativeChartsDropDownHandler.bind(this);
     this.chartTopActorsByLikes = this.chartTopActorsByLikes.bind(this);
+    this.absoluteChartsDropDownHandler = this.absoluteChartsDropDownHandler.bind(this);
+    // this.relativeChartsDropDownHandler = this.relativeChartsDropDownHandler.bind(this);
   }
 
   componentWillMount() {
@@ -220,6 +222,7 @@ class Dashboard extends React.Component {
         </div>
         <br />
         <div className="row">
+<<<<<<< HEAD
           { this.state.topTagsByName &&
             this.state.mostSelectedTagNames &&
             this.state.topActors ?
@@ -236,6 +239,19 @@ class Dashboard extends React.Component {
             </div>
             : <h1 className="col-sm-10">Loading your profile data...</h1>
           }
+=======
+          <DropDownMenu
+            onSelect={this.absoluteChartsDropDownHandler}
+          />
+          {this.state.topTagsByName && <PieChart
+            labels={this.state.topTagsByName}
+            data={this.state.topTagPicksCountsByUser}
+          />}
+          {this.state.mostSelectedTagNames && <BarChart
+            labels={this.state.mostSelectedTagNames}
+            data={this.state.mostSelectedTagPercentages}
+          />}
+>>>>>>> feat($dashboard): Display dropdown menu for charts; component remains unconnected
         </div>
       </div>
     );
