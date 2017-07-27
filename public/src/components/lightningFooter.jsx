3ce027c2c742dc720e38c5538786c201170f4926
Andrew Shih <acs@Andrews-MacBook-Pro.local>
2017-07-27 10:36:34 -0400

    style($dashboard): Add colors to dashboard charts

--- public/src/components/lightningFooter.jsx
+++ public/src/components/lightningFooter.jsx
@@ -5,11 +5,11 @@ const LightningFooter = props => (
   <div className="row">
     <div className="col-sm-12">
       <div className="lightning-footer">
-        <h3 className="col-sm-6">{`When you want to see FLIQ's suggestions, click here: `}</h3>
-        <div className="col-sm-6">
+        {/* <h3 className="col-sm-6">{`When you want to see FLIQ's suggestions, click here: `}</h3> */}
+        <div className="col-sm-12">
           <Link to="/results">
             <button
-              className="btn btn-lg btn-primary fliq-button pull-left"
+              className="btn btn-lg btn-primary fliq-button see-suggestions-btn"
             >
               See Suggestions
             </button>
